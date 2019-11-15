f3a2605f568494708f4125a500931e9ea8a68b1a
unjamini <rumyantseva2000@gmail.com>
2019-11-15 13:47:45 +0300

    11

--- filterSeq.cpp
+++ filterSeq.cpp
@@ -29,24 +29,26 @@ void FilterSeq::parseConfig(std::string const &configName)
     std::ifstream confStream(configName);
     if (confStream.is_open()) {
         while (getline(confStream, line)) {
-            std::istringstream iss(line);
-            std::vector<std::string> parsed_line((std::istream_iterator<std::string>(iss)),
-                                             std::istream_iterator<std::string>());
-            if (parsed_line[0] == "Red") {
-                FilterUnit* flt = new RedFilter(stoi(parsed_line[1]), stoi(parsed_line[3]), stoi(parsed_line[2]), stoi(parsed_line[4]));
-                filterSeq.push_back(flt);
-            }
-            else if (parsed_line[0] == "Threshold") {
-                FilterUnit* flt = new ThresholdFilter(stoi(parsed_line[1]), stoi(parsed_line[3]), stoi(parsed_line[2]), stoi(parsed_line[4]));
-                filterSeq.push_back(flt);
-            }
-            else if (parsed_line[0] == "Edge") {
-                FilterUnit* flt = new EdgeFilter(stoi(parsed_line[1]), stoi(parsed_line[3]), stoi(parsed_line[2]), stoi(parsed_line[4]));
-                filterSeq.push_back(flt);
-            }
-            else if (parsed_line[0] == "Blur") {
-                FilterUnit* flt = new BlurFilter(stoi(parsed_line[1]), stoi(parsed_line[3]), stoi(parsed_line[2]), stoi(parsed_line[4]));
-                filterSeq.push_back(flt);
+            if (!line.empty()) {
+                std::istringstream iss(line);
+                std::vector<std::string> parsed_line((std::istream_iterator<std::string>(iss)),
+                                                     std::istream_iterator<std::string>());
+                if (parsed_line[0] == "Red") {
+                    FilterUnit* flt = new RedFilter(stoi(parsed_line[1]), stoi(parsed_line[3]), stoi(parsed_line[2]), stoi(parsed_line[4]));
+                    filterSeq.push_back(flt);
+                }
+                else if (parsed_line[0] == "Threshold") {
+                    FilterUnit* flt = new ThresholdFilter(stoi(parsed_line[1]), stoi(parsed_line[3]), stoi(parsed_line[2]), stoi(parsed_line[4]));
+                    filterSeq.push_back(flt);
+                }
+                else if (parsed_line[0] == "Edge") {
+                    FilterUnit* flt = new EdgeFilter(stoi(parsed_line[1]), stoi(parsed_line[3]), stoi(parsed_line[2]), stoi(parsed_line[4]));
+                    filterSeq.push_back(flt);
+                }
+                else if (parsed_line[0] == "Blur") {
+                    FilterUnit* flt = new BlurFilter(stoi(parsed_line[1]), stoi(parsed_line[3]), stoi(parsed_line[2]), stoi(parsed_line[4]));
+                    filterSeq.push_back(flt);
+                }
             }
         }
         confStream.close();
